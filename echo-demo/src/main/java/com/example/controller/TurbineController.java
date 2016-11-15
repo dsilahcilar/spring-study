f4cd5d245cdec7d93a559d0bd50341e485a11d5d
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-11-15 20:07:34 +0200

    controllers.

--- echo-demo/src/main/java/com/example/controller/TurbineController.java
+++ echo-demo/src/main/java/com/example/controller/TurbineController.java
@@ -1,26 +1,45 @@
 package com.example.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
 import com.example.ApplicationProperties;
 import com.example.model.TurbineRequestModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
-@RequestMapping("/turbine/config")
+@RequestMapping("/")
 public class TurbineController {
-	@Autowired
-	private RestTemplate restTemplate;
-	@Autowired
-	private ApplicationProperties applicationProperties;
-	
-	@RequestMapping(path = "/", method = RequestMethod.POST)
-	public TurbineRequestModel turbine(@RequestBody TurbineRequestModel request ) {
-		Object response = restTemplate.postForEntity(applicationProperties.getUrl() + "/turbine/", request, Object.class);
-		return request;
-	}
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
+    @RequestMapping(path = "/turbine/config", method = RequestMethod.POST)
+    public TurbineRequestModel turbine(@RequestBody TurbineRequestModel request) {
+        Object response = restTemplate.postForEntity(applicationProperties.getUrl() + "/turbines", request, Object.class);
+        return request;
+    }
+
+
+    @RequestMapping(path = "/turbines", method = RequestMethod.GET)
+    public List<TurbineRequestModel> list() {
+        ResponseEntity<TurbineRequestModel[]> response = restTemplate.getForEntity(applicationProperties.getUrl() + "/turbines", TurbineRequestModel[].class);
+        return Arrays.asList(response.getBody());
+    }
+
+    @RequestMapping(path = "/turbines/{turbineName}", method = RequestMethod.POST)
+    public void start(@PathVariable String turbineName) {
+        restTemplate.postForEntity(applicationProperties.getUrl() + "/turbines/" + turbineName, null, TurbineRequestModel[].class);
+    }
+
+    @RequestMapping(path = "/turbines/{turbineName}", method = RequestMethod.DELETE)
+    public void stop(@PathVariable String turbineName) {
+        restTemplate.delete(applicationProperties.getUrl() + "/turbines/" + turbineName, TurbineRequestModel[].class);
+    }
+
+
 }
