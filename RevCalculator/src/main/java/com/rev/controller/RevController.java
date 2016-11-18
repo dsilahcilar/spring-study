2b0ca0c23a0e4395fd26e6a6e5f6d09737720eea
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-11-18 19:36:25 +0200

    sum updates

--- RevCalculator/src/main/java/com/rev/controller/RevController.java
+++ RevCalculator/src/main/java/com/rev/controller/RevController.java
@@ -32,7 +32,9 @@ public class RevController {
     public ResponseType calculateRevenue(@RequestBody TurbineOutputParameters request) {
         System.out.println("TurbineOutputParameters  " + request.toString());
         revCalculatorService.calculateRevenue(request);
-        Message message = new Message(request.getName(), request.getPowerProd(), Calendar.getInstance().getTime().getTime());
+        SumModel sumModel = revCalculatorService.sumByName(request.getName());
+        Message message = new Message(request.getName(), request.getPowerProd(),
+                Calendar.getInstance().getTime().getTime(), sumModel.getRevenue(), sumModel.getProd());
         restTemplate.postForEntity(applicationProperties.getUrl() + "/turbines/websocket", message, Object.class);
         return new ResponseType("Ok", LocalDateTime.now());
     }
--- config-service/src/main/java/com/example/model/Message.java
+++ config-service/src/main/java/com/example/model/Message.java
@@ -4,14 +4,34 @@ public class Message {
     private String turbineName;
     private Integer prod;
     private Long time;
+    private Double sumRevenue;
+    private Long sumProd;
 
     public Message() {
     }
 
-    public Message(String turbineName,Integer prod, Long time) {
+    public Message(String turbineName, Integer prod, Long time, Double sumRevenue, Long sumProd) {
         this.turbineName = turbineName;
         this.prod = prod;
         this.time = time;
+        this.sumRevenue = sumRevenue;
+        this.sumProd = sumProd;
+    }
+
+    public Double getSumRevenue() {
+        return sumRevenue;
+    }
+
+    public void setSumRevenue(Double sumRevenue) {
+        this.sumRevenue = sumRevenue;
+    }
+
+    public Long getSumProd() {
+        return sumProd;
+    }
+
+    public void setSumProd(Long sumProd) {
+        this.sumProd = sumProd;
     }
 
     public String getTurbineName() {
